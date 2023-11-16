34ec8a595805143ab5ee2b1f85ffcee858ef0144
Serg <sergey.gabrielyan1998@gmail.com>
2023-11-16 21:49:45 +0400

    #5 Add font size in tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 function App() {
-  return <div className="text-primary-red-45">App</div>;
+  return (
+    <div>
+      <h1 className="text-7xl text-primary-red-45">App</h1>
+      <p className="text-lg">
+        Illum tenetur quos eaque. Ornare potenti nobis saepe rutrum! Imperdiet
+        cubilia tenetur? Eu nostra ex maxime tempus nonummy? Tortor nesciunt
+        turpis? Sagittis venenatis! Cubilia? Lectus quas laboriosam magna
+        imperdiet incidunt, alias sapien occaecat sequi, magnis molestiae?
+        Cillum habitasse laudantium asperiores. In voluptate, et senectus,
+        perspiciatis maxime consequat eiusmod explicabo, alias congue quod!
+        Natoque iste. Convallis autem. Feugiat suspendisse blandit commodo
+        dolore, dictumst nunc, sequi sint laborum unde cubilia convallis
+        excepteur, aliquid gravida unde neque? Inventore, elementum minim,
+        molestiae adipisci repudiandae venenatis! Tortor ut perspiciatis libero
+        luctus, eu praesent at augue, eos dictumst blandit mi adipiscing earum
+        accusantium, voluptas dignissimos mi.
+      </p>
+    </div>
+  );
 }
 
 export default App;
