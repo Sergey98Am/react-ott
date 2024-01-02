362e8a03cf2e563d55bfdd486fb577fdac6c14bc
Serg <sergey.gabrielyan1998@gmail.com>
2024-01-02 18:55:42 +0400

    #32 Add circle variant for badge

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -3,19 +3,19 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
-const badgeVariants = cva(
-  "w-auto text-center !font-semibold rounded p-2 text-xs 2xl:p-2.5 2xl:text-base 2xl:rounded-md",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary-red-45 text-absolute-white",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
+const badgeVariants = cva("w-auto text-center", {
+  variants: {
+    variant: {
+      default:
+        "bg-primary-red-45 text-absolute-white !font-semibold rounded p-2 text-xs 2xl:p-2.5 2xl:text-base 2xl:rounded-md",
+      circle:
+        "bg-dark-black-08 border border-dark-black-15 p-1 text-xs 2xl:text-base",
     },
   },
-);
+  defaultVariants: {
+    variant: "default",
+  },
+});
 
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
