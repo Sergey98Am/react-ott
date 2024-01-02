a8d56cc3ee5381d4d1b10aaa6397fe72e9c00102
Serg <sergey.gabrielyan1998@gmail.com>
2024-01-02 14:09:45 +0400

    #30 Add Badge component and use in genre card

---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "w-auto text-center !font-semibold rounded p-2 text-xs 2xl:p-2.5 2xl:text-base 2xl:rounded-md",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary-red-45 text-absolute-white",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
