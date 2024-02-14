544241f96119ae6ab1ac3a279b3884147152dab1
Serg <sergey.gabrielyan1998@gmail.com>
2024-02-15 02:21:52 +0400

    #66 Remove unused code from Subscriptions component

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,11 +1,5 @@
-import img from "../assets/3474c3c97f3e4f39bd24899b93d094e9.png";
-
 const Subscriptions = () => {
-  return (
-    <div>
-      <img src={img} alt="" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Subscriptions;
