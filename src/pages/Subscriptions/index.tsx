a30a027b919ac5b7572aa43dd156641cbc73f105
Serg <sergey.gabrielyan1998@gmail.com>
2024-02-18 20:41:24 +0400

    #70 Add reusable components in subscriptions page

---
+++ src/pages/Subscriptions/index.tsx
@@ -0,0 +1,13 @@
+import FreeTrial from "@/components/FreeTrial";
+import Pricing from "@/components/Pricing";
+import Container from "@/components/ui/Container";
+const Subscriptions = () => {
+  return (
+    <Container className="space-y-24 pb-20 pt-36 md:pb-28 2xl:space-y-32 2xl:pt-40">
+      <Pricing />
+      <FreeTrial />
+    </Container>
+  );
+};
+
+export default Subscriptions;
